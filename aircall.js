c4cda296e2b03f4d70c6e77fff0ff114077b4429
Redha Juanda <redhajuanda@gmail.com>
2019-01-29 12:32:57 +0700

    Config Heroku

--- aircall.js
+++ aircall.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port;
 const request = require('request');
 const config = require('./config');
 const model = require('./model');
